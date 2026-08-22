ea87934c01d7435120cfb6b4529be3914f7a0bcb
Elia Magini <elia.magini@studenti.unicam.it>
2026-08-22 16:43:08 +0200

    docs: add documentation for NPC, Personaggio

---
+++ src/main/java/it/unicam/cs/mpgc/rpg129097/model/NPC.java
@@ -0,0 +1,58 @@
+package it.unicam.cs.mpgc.rpg129097.model;
+
+import it.unicam.cs.mpgc.rpg129097.interfaces.Validatore;
+import lombok.Getter;
+import lombok.NonNull;
+import lombok.Setter;
+
+/**
+ * Rappresenta un personaggio non giocante (NPC) all'interno del gioco.
+ * <p>Un NPC è un {@link Personaggio} caratterizzato da un oggetto {@link Validatore},
+ * che rappresenta l'oggetto o elemento associato al personaggio, e da un livello
+ * di intuizione.</p>
+ *
+ * @author Elia Magini
+ */
+public class NPC extends Personaggio {
+
+    @Getter
+    @Setter
+    private Validatore item;
+
+
+    @Getter
+    private String intuizione;
+
+    /**
+     * Costruisce un nuovo NPC.
+     * @param nome nome dell'NPC
+     * @param professione professione dell'NPC
+     * @param item oggetto associato all'NPC
+     * @param intuizione informazione o indizio fornito dall'NPC
+     * @throws NullPointerException se {@code nome} o {@code professione} sono {@code null}
+     */
+    public NPC(@NonNull String nome, @NonNull String professione,
+               Validatore item, String intuizione) {
+        super(nome, professione);
+        this.item = item;
+        this.intuizione = intuizione;
+    }
+
+    /**
+     * Stampa la presentazione dell'NPC.
+     * <p>La presentazione viene ottenuta tramite il metodo
+     * {@link #getPresentazione()}.</p>
+     */
+    @Override
+    public void presentati() {
+        System.out.println(getPresentazione());
+    }
+
+    /**
+     * Restituisce la presentazione dell'NPC.
+     * @return la stringa contenente la presentazione dell'NPC
+     */
+    public String getPresentazione() {
+        return "";
+    }
+}
--- src/main/java/it/unicam/cs/mpgc/rpg129097/model/Personaggio.java
+++ src/main/java/it/unicam/cs/mpgc/rpg129097/model/Personaggio.java
@@ -1,21 +1,41 @@
 package it.unicam.cs.mpgc.rpg129097.model;
 
-
 import lombok.Getter;
 import lombok.NonNull;
 
+/**
+ * Rappresenta l'astrazione di un personaggio giocante o non giocante
+ * all'interno del gioco.
+ *
+ * <p>Ogni personaggio è caratterizzato da un nome e da una professione,
+ * definisce inoltre il metodo astratto presentati()</p>
+ *
+ * @author Elia Magini
+ */
 public abstract class Personaggio {
+
     @Getter
     protected String nome;
+
     @Getter
     protected String professione;
 
-
-    protected Personaggio(@NonNull String nome, @NonNull String professione){
+    /**
+     * Costruisce un nuovo Personaggio.
+     * @param nome nome del personaggio
+     * @param professione professione del personaggio
+     * @throws NullPointerException se {@code nome} o {@code professione}
+     *         sono {@code null}
+     */
+    protected Personaggio(@NonNull String nome, @NonNull String professione) {
         this.nome = nome;
         this.professione = professione;
     }
 
+    /**
+     * Presenta il personaggio.
+     * <p>L'implementazione della presentazione è demandata alle classi
+     * concrete che estendono {@code Personaggio}.</p>
+     */
     public abstract void presentati();
-
 }
