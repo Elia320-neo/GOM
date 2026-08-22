247a643674230027c092bb7e67167188f23ace70
Elia Magini <elia.magini@studenti.unicam.it>
2026-08-22 16:22:06 +0200

    feat: add Item

--- src/main/java/it/unicam/cs/mpgc/rpg129097/model/Item.java
+++ src/main/java/it/unicam/cs/mpgc/rpg129097/model/Item.java
@@ -1,4 +1,30 @@
 package it.unicam.cs.mpgc.rpg129097.model;
 
-public class Item {
+import it.unicam.cs.mpgc.rpg129097.interfaces.Proposizione;
+import it.unicam.cs.mpgc.rpg129097.interfaces.Validatore;
+import lombok.Getter;
+import lombok.NonNull;
+import lombok.Setter;
+
+public class Item implements Validatore {
+    @Getter
+    private String nome;
+    @Getter
+    private String descrizione;
+    @Getter @Setter
+    private boolean utilizzato;
+    private final ArchivioProposizioni archivio; //dipendenza di utilizzo
+
+    public Item(@NonNull String nome, @NonNull String descrizione, ArchivioProposizioni archivio){
+        this.nome = nome;
+        this.descrizione = descrizione;
+        this.archivio = archivio;
+    }
+
+
+    @Override
+    public Proposizione valida(String intuizione) {
+        return archivio.cerca(intuizione);
+    }
+
 }
