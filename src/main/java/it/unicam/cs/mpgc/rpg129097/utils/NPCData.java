94f68208da388e4c74020860149a0bbbce94c287
Elia Magini <elia.magini@studenti.unicam.it>
2026-08-23 11:13:05 +0200

    feat: add NPCData

---
+++ src/main/java/it/unicam/cs/mpgc/rpg129097/utils/NPCData.java
@@ -0,0 +1,14 @@
+package it.unicam.cs.mpgc.rpg129097.utils;
+
+
+/**
+ * Rappresenta un Data Transfer Object (DTO) utilizzato per
+ * rappresentare e deserializzare i dati grezzi di un NPC
+ * da una fonte esterna, come un file JSON.
+ */
+public class NPCData {
+    public String nome;
+    public String professione;
+    public String itemNome;
+    public String itemDescrizione;
+}
