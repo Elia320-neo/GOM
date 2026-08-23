ec27879adc432660e1583ebc64c4b87551562cd3
Elia Magini <elia.magini@studenti.unicam.it>
2026-08-23 10:53:01 +0200

    docs: add javadoc ParserProposizioni

--- src/main/java/it/unicam/cs/mpgc/rpg129097/utils/ParserProposizioni.java
+++ src/main/java/it/unicam/cs/mpgc/rpg129097/utils/ParserProposizioni.java
@@ -13,11 +13,26 @@ import java.lang.reflect.Type;
 import java.util.Collection;
 import java.util.List;
 
+/**
+ * Rappresenta una classe di utilità per il parsing
+ * e il caricamento delle proposizioni da un file di risorsa JSON.
+ *  @author Elia Magini
+ */
 public class ParserProposizioni {
 
+    /**
+     * Legge un file JSON situato nei {@code resources} del progetto e converte i suoi dati
+     * in un'istanza di {@link ArchivioProposizioni}.
+     * @param resourcePath il percorso relativo della risorsa JSON nel classpath
+     * @return un'istanza di {@link ArchivioProposizioni} contenente le proposizioni caricate
+     * @throws RuntimeException se il file non viene trovato nel percorso specificato
+     * o se si verifica un errore durante la lettura o deserializzazione del JSON
+     */
     public ArchivioProposizioni parse(String resourcePath) {
         try (InputStream is = getClass().getClassLoader().getResourceAsStream(resourcePath)) {
-            if (is == null) {throw new RuntimeException("Risorsa non trovata: " + resourcePath);}
+            if (is == null) {
+                throw new RuntimeException("Risorsa non trovata: " + resourcePath);
+            }
 
             Reader reader = new InputStreamReader(is);
             Gson gson = new Gson();
