a4fce58edb305ca21b70ef51466a948ca4902e9f
Elia Magini <elia.magini@studenti.unicam.it>
2026-08-23 11:09:29 +0200

    test: update main() to test ParserProposizioni

--- src/main/java/it/unicam/cs/mpgc/rpg129097/MainApp.java
+++ src/main/java/it/unicam/cs/mpgc/rpg129097/MainApp.java
@@ -1,7 +1,24 @@
 package it.unicam.cs.mpgc.rpg129097;
 
+import it.unicam.cs.mpgc.rpg129097.interfaces.Proposizione;
+import it.unicam.cs.mpgc.rpg129097.model.ArchivioProposizioni;
+import it.unicam.cs.mpgc.rpg129097.utils.ParserProposizioni;
+
 public class MainApp {
-    public static void main() {
-        System.out.println("Hello world");
+
+    public static void main(String[] args) {
+        ArchivioProposizioni archivio = new ParserProposizioni().parse("archivio_esiti.json");
+
+        System.out.println("Proposizioni caricate: " + archivio.getProposizioni().size());
+
+        for (Proposizione p : archivio.getProposizioni()) {
+            System.out.printf("- \"%s\" -> esito: %b%n", p.getDescrizione(), p.getEsito());
+        }
+
+        System.out.println("----------------------------------------");
+
+        String descrizioneTest = "La verità è che tu sei il male";
+        Proposizione trovata = archivio.cerca(descrizioneTest);
+        System.out.println("Ricerca \"" + descrizioneTest + "\" -> esito: " + trovata.getEsito());
     }
 }
