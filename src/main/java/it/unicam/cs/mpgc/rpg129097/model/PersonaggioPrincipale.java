206565a5a6a3d758940b3929a070633f0266e113
Elia Magini <elia.magini@studenti.unicam.it>
2026-08-23 11:51:52 +0200

    feat: add valutaLivello()

--- src/main/java/it/unicam/cs/mpgc/rpg129097/model/PersonaggioPrincipale.java
+++ src/main/java/it/unicam/cs/mpgc/rpg129097/model/PersonaggioPrincipale.java
@@ -8,6 +8,7 @@ import lombok.NonNull;
 public class PersonaggioPrincipale extends Personaggio {
 
     private static final int PUNTI_SCOMMESSA = 10;
+    private static final int[] SOGLIE_ESPERIENZA_PER_LIVELLO = {0, 10, 30};
 
     private int esperienza;
     private int livello;
@@ -34,8 +35,6 @@ public class PersonaggioPrincipale extends Personaggio {
     public void addValidazione(){this.validazioniEffettuate++;}
 
 
-
-
     public boolean fidati(NPC npc) {
         boolean esito = verifica(npc);
         if (esito) {
@@ -59,6 +58,16 @@ public class PersonaggioPrincipale extends Personaggio {
     private boolean verifica(NPC npc) {
         addValidazione();
         return npc.getItem().valida(npc.getIntuizione()).getEsito();
+    }
+
+    public void valutaLivello(int giornataCompletata) {
+        if (giornataCompletata < 1 || giornataCompletata > SOGLIE_ESPERIENZA_PER_LIVELLO.length) {
+            throw new IllegalArgumentException("Giornata non valida: " + giornataCompletata);
+        }
+        int sogliaRichiesta = SOGLIE_ESPERIENZA_PER_LIVELLO[giornataCompletata - 1];
+        if (esperienza >= sogliaRichiesta) {
+            livello = Math.max(livello, giornataCompletata);
+        }
     }
 
 }
