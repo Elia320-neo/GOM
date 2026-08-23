431542b830e2e4d524446d16b47cb857aea0f450
Elia Magini <elia.magini@studenti.unicam.it>
2026-08-23 10:29:26 +0200

    chore: add gson dependecies

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,6 +9,7 @@ repositories {
 dependencies {
     compileOnly("org.projectlombok:lombok:1.18.46")
     annotationProcessor("org.projectlombok:lombok:1.18.46")
+    implementation("com.google.code.gson:gson:2.13.2")
 }
 
 application {
---
+++ src/main/java/it/unicam/cs/mpgc/rpg129097/utils/ParserProposizioni.java
@@ -0,0 +1,4 @@
+package it.unicam.cs.mpgc.rpg129097.utils;
+
+public class ParserProposizioni {
+}
