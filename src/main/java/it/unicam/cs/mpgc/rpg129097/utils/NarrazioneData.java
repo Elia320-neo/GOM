3f9a53ea382563419e6f21c505ac094578c63cd5
Elia Magini <elia.magini@studenti.unicam.it>
2026-08-24 10:20:47 +0200

    feat: add NarrazioneData & javadoc

---
+++ src/main/java/it/unicam/cs/mpgc/rpg129097/utils/NarrazioneData.java
@@ -0,0 +1,13 @@
+package it.unicam.cs.mpgc.rpg129097.utils;
+
+
+/**
+ * Rappresenta un Data Transfer Object (DTO) utilizzato per
+ * deserializzare i dati grezzi di un file json contenente
+ * i testi per la narrazione del gioco.
+ * @author Elia Magini
+ */
+public class NarrazioneData {
+    public int numeroGiornata;
+    public String descrizione;
+}
