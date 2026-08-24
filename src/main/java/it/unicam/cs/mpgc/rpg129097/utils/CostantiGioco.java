9c8992bef83d250e570857af592499ed21ee78fd
Elia Magini <elia.magini@studenti.unicam.it>
2026-08-24 11:51:19 +0200

    feat: add NPC_JSON

--- src/main/java/it/unicam/cs/mpgc/rpg129097/utils/CostantiGioco.java
+++ src/main/java/it/unicam/cs/mpgc/rpg129097/utils/CostantiGioco.java
@@ -4,4 +4,5 @@ public final class CostantiGioco {
     public static final int NPC_PER_GIORNATA = 3;
     public static final int NUMERO_GIORNATE = 3;
     public static final int NPC_TOTALI = NPC_PER_GIORNATA * NUMERO_GIORNATE;
+    public static final String NPC_JSON = "npc.json";
 }
