9150d9638e28bb387a1c9a8f9f22828900123490
Elia Magini <elia.magini@studenti.unicam.it>
2026-08-24 11:31:47 +0200

    feat: add CostantiGioco

---
+++ src/main/java/it/unicam/cs/mpgc/rpg129097/utils/CostantiGioco.java
@@ -0,0 +1,7 @@
+package it.unicam.cs.mpgc.rpg129097.utils;
+
+public final class CostantiGioco {
+    public static final int NPC_PER_GIORNATA = 3;
+    public static final int NUMERO_GIORNATE = 3;
+    public static final int NPC_TOTALI = NPC_PER_GIORNATA * NUMERO_GIORNATE;
+}
