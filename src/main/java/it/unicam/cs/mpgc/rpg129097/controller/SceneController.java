a2eea1305b98df3d33b09a1f1f7fa3fdf511015f
Elia Magini <elia.magini@studenti.unicam.it>
2026-08-25 15:45:21 +0200

    feat: add SceneController

---
+++ src/main/java/it/unicam/cs/mpgc/rpg129097/controller/SceneController.java
@@ -0,0 +1,32 @@
+package it.unicam.cs.mpgc.rpg129097.controller;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import lombok.Setter;
+
+public class SceneController {
+    @Setter
+    private static Stage stage;
+
+    public static void cambia(String fxml, Object data) {
+        try {
+            FXMLLoader loader = new FXMLLoader(SceneController.class.getResource("/fxml/" + fxml));
+            Parent root = loader.load();
+
+            Object controller = loader.getController();
+            if (data != null) {
+                controller.getClass().getMethod("setPartita", data.getClass()).invoke(controller, data);
+            }
+
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
