3982fcaa9ad2869a5f9b81888c259f1d5736db82
Elia Magini <elia.magini@studenti.unicam.it>
2026-08-25 19:15:49 +0200

    docs: javadoc

--- src/main/java/it/unicam/cs/mpgc/rpg129097/MainApp.java
+++ src/main/java/it/unicam/cs/mpgc/rpg129097/MainApp.java
@@ -8,8 +8,23 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+/**
+ * Classe principale dell'applicazione JavaFX.
+ * <p>Si occupa di inizializzare l'applicazione, configurare la finestra
+ * principale e caricare la schermata iniziale del gioco.</p>
+ *
+ * @author Elia Magini
+ */
 public class MainApp extends Application {
 
+    /**
+     * Avvia l'applicazione JavaFX.
+     * <p>Imposta il {@link Stage} principale nel {@link SceneController},
+     * carica il file FXML della schermata iniziale e configura le
+     * dimensioni e le proprietà della finestra.</p>
+     * @param stage il {@link Stage} principale dell'applicazione
+     * @throws Exception se si verifica un errore durante il caricamento del file FXML
+     */
     @Override
     public void start(Stage stage) throws Exception {
         SceneController.setStage(stage);
@@ -25,6 +40,11 @@ public class MainApp extends Application {
         stage.show();
     }
 
+    /**
+     * Metodo principale dell'applicazione.
+     * <p>Avvia il framework JavaFX tramite il metodo launch().</p>
+     * @param args argomenti passati da riga di comando
+     */
     public static void main(String[] args) {
         launch();
     }
