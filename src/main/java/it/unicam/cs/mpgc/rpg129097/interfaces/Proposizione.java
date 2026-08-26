5595f412cb0347fb3375fbb473c5afc87299d378
Elia Magini <elia.magini@studenti.unicam.it>
2026-08-26 11:41:13 +0200

    docs: add javadoc

--- src/main/java/it/unicam/cs/mpgc/rpg129097/interfaces/Proposizione.java
+++ src/main/java/it/unicam/cs/mpgc/rpg129097/interfaces/Proposizione.java
@@ -1,6 +1,23 @@
 package it.unicam.cs.mpgc.rpg129097.interfaces;
 
+/**
+ * Rappresenta una proposizione del gioco.
+ * <p>Una proposizione è caratterizzata da una descrizione testuale
+ * e da un esito booleano che ne rappresenta la validità.</p>
+ *
+ * @author Elia Magini
+ */
 public interface Proposizione {
+
+    /**
+     * Restituisce la descrizione della proposizione.
+     * @return descrizione testuale della proposizione
+     */
     String getDescrizione();
+
+    /**
+     * Restituisce l'esito della proposizione.
+     * @return {@code true} se la proposizione ha esito positivo, {@code false} altrimenti
+     */
     boolean getEsito();
 }
--- src/main/java/it/unicam/cs/mpgc/rpg129097/interfaces/Validatore.java
+++ src/main/java/it/unicam/cs/mpgc/rpg129097/interfaces/Validatore.java
@@ -1,7 +1,32 @@
 package it.unicam.cs.mpgc.rpg129097.interfaces;
 
+/**
+ * Rappresenta un elemento in grado di validare un'intuizione
+ * fornita dal giocatore.
+ * <p>Un validatore possiede un nome e una descrizione e permette
+ * di verificare un'intuizione restituendo una {@link Proposizione}
+ * che ne rappresenta l'esito.</p>
+ *
+ * @author Elia Magini
+ */
 public interface Validatore {
+
+    /**
+     * Restituisce il nome del validatore.
+     * @return nome del validatore
+     */
     String getNome();
+
+    /**
+     * Restituisce la descrizione del validatore.
+     * @return descrizione del validatore
+     */
     String getDescrizione();
+
+    /**
+     * Valida un'intuizione fornita dal giocatore.
+     * @param intuzione intuizione da validare
+     * @return proposizione contenente la descrizione e l'esito della validazione
+     */
     Proposizione valida(String intuzione);
 }
