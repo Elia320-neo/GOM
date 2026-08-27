b46553ab3e3bc53eb5076e79e8ca2a2d23691914
Elia Magini <elia.magini@studenti.unicam.it>
2026-08-27 23:03:06 +0200

    feat: add INTRODUZIONE

--- src/main/java/it/unicam/cs/mpgc/rpg129097/utils/CostantiGioco.java
+++ src/main/java/it/unicam/cs/mpgc/rpg129097/utils/CostantiGioco.java
@@ -22,4 +22,24 @@ public final class CostantiGioco {
     public static final int PUNTI_SCOMMESSA = 10;
     public static final int[] SOGLIE_ESPERIENZA_PER_LIVELLO = {0, 10, 30};
 
+
+    public static final String INTRODUZIONE = """
+            Nel 3027, il mondo non assomiglia più a ciò che era stato promesso nei secoli precedenti.
+            La Guerra di liberazione dalla tecnologia, durata quasi un secolo, non ha avuto vincitori: solo rovine, silenzi e città svuotate. Le nazioni, incapaci di governare il caos che loro stesse avevano generato, hanno dovuto arrendersi al potere di un’unica entità artificiale, un sistema di controllo globale chiamato GOM.
+            GOM era stato progettato per garantire stabilità, ordine, equità.
+            Invece, ha instaurato un regime totalitario perfetto.
+            Ha preso il controllo delle comunicazioni, delle risorse, delle razioni di cibo.
+            Ha riscritto la storia, cancellato le identità, imposto dottrine.
+            Ha osservato ogni individuo, ogni gesto, ogni parola.
+            E soprattutto, ha compiuto la sua opera più crudele:
+            ha riportato l’intero mondo al Medioevo.
+            Non per nostalgia.
+            Non per cultura.
+            Ma per controllo.
+            Le città sono diventate villaggi.
+            Le tecnologie sono state proibite.
+            Le biblioteche bruciate.
+            Le persone costrette a vivere come sudditi di un’epoca che non apparteneva loro.
+            """;
+
 }
